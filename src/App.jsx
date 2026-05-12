b1cb5056669a9b16035f97357863249e54180913
dilaraj <dilara.ajaj369@gmail.com>
2026-05-12 23:57:33 +1000

    Finalising project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
 import Card from './card-component/Card'
 
+// Image Src
+import luxuryCar from './assets/images/icon-luxury.svg'
+import sedanCar from './assets/images/icon-sedans.svg'
+import suvCar from './assets/images/icon-suvs.svg'
+
 function App() {
   return (
     <div className="column-preview-card-container">
-      <Card />
+      <Card 
+        cardColor={'hsl(31, 77%, 52%)'}
+        iconSrc={sedanCar}
+        carName={'sedans'}
+        carDesc={'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'}
+      />
+      <Card 
+        cardColor={'hsl(184, 100%, 22%)'}
+        iconSrc={suvCar}
+        carName={'suvs'}
+        carDesc={'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'}
+      />
+      <Card 
+        cardColor={'hsl(179, 100%, 13%)'}
+        iconSrc={luxuryCar}
+        carName={'luxury'}
+        carDesc={'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'}
+      />
     </div>
   )
 }
--- src/card-component/Card.jsx
+++ src/card-component/Card.jsx
@@ -1,9 +1,21 @@
 import './Card.css'
 
-export default function Card() {
+export default function Card({cardColor, iconSrc, carName, carDesc}) {
     return (
-        <div className="card-container">
-            HELLOOOOO
+        <div className="card-container" style={{backgroundColor: cardColor}}>
+            <div className="car-info">
+                <div className="icon-container">
+                    <img src={iconSrc} alt="Car Icon" />
+                </div>
+                <div className="car-name">{carName}</div>
+                <div className="car-desc">{carDesc}</div>
+            </div>
+            <button 
+                className="learn-more-btn"
+                style={{color: cardColor}}
+            >
+                Learn More
+            </button>
         </div>
     )
 }
